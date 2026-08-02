a2e4bfd096dfe5c2f90dad826c7eadae0197df7e
yash <sauravk0257@gmail.com>
2026-08-02 05:56:24 +0530

    Add files via upload

---
+++ doommusic/utils/decorators/__init__.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------
+# 🔸 Doom Music Project
+# 🔹 Developed & Maintained by: Yash (https://github.com/yashcodex121)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by Yash
+# -----------------------------------------------
+from .admins import *
+from .language import *
